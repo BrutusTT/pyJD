2fb24611cdfa791889070f61fe5860b06c69a39b
BrutusTT <brutusthetschiepel@gmail.com>
2016-09-12 18:02:41 +0100

    added a lookAt port to JDModule
    makes the JDLookAtModule obsolete

--- pyJD/JDModule.py
+++ pyJD/JDModule.py
@@ -16,10 +16,38 @@
 #    along with pyJD.  If not, see <http://www.gnu.org/licenses/>.                                 #
 ####################################################################################################
 from pyJD.EZModule  import EZModule, main
+from pyJD.utils     import rad2deg, angle
 
 
 class JDModule(EZModule):
     """ The JDModule class provides a yarp module to control the JD robots motors. """
+
+
+    CUR_Z = 0.0
+    CUR_X = 0.0
+    CUR_Y = 0.0
+
+
+    def configure(self, rf):
+
+
+        if not hasattr(self, 'lookAtPort'):
+            EZModule.configure(self, rf)
+            self.lookAtPort = self.createInputPort('lookAt', 'buffered')
+
+        return True
+
+
+    
+    def updateModule(self):
+        
+        # read the bottle              
+        lookAt_bottle    = self.lookAtPort.read()
+        if lookAt_bottle:
+            self.lookAt(lookAt_bottle)
+
+        return True
+
 
 
     def respond(self, command, reply):
@@ -45,6 +73,34 @@ class JDModule(EZModule):
         reply.addString('nack')
         return True
 
+
+    def lookAt(self, bottle):
+        """ This method executes the lookAt command.
+
+        @param bottle - input command
+        """
+
+        # get the coordinates from the bottle
+        near_far    = bottle.get(1).asDouble() * -1.0
+        left_right  = bottle.get(2).asDouble()
+        down_up     = bottle.get(3).asDouble()
+
+        if bottle.size() > 3:
+            near_far    += self.CUR_Z
+            left_right  += self.CUR_X
+            down_up     += self.CUR_Y
+
+        # calculate the angles
+        angle_d0 = rad2deg( angle([1.0, 0.0, 0.0], [left_right, down_up, near_far]) )
+        angle_d1 = rad2deg( angle([0.0, 1.0, 0.0], [left_right, down_up, near_far]) )
+
+        # send it to the motors
+        self.sendPosition(0, angle_d0 + 20)
+        self.sendPosition(1, angle_d1)
+
+        self.CUR_Z = near_far
+        self.CUR_X = left_right
+        self.CUR_Y = down_up
 
 
 
